540ac6a70348a4ddc5603ac3a360c466ad88c3f4
najubudeen <najubudeenktm@gmail.com>
2025-03-05 11:46:10 +0530

    third commit

--- build/slider-working-test/index.asset.php
+++ build/slider-working-test/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c7c0f9db420ef79b4bc2');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a9bb4d2cefb01ea79a23');
